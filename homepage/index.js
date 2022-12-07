be24518f5e23f4b91d4aa8e908e9d58699a84b12
yasss <88993103+SEGITIGaa@users.noreply.github.com>
2022-12-07 19:41:36 +0700

    Delete homepage directory

--- homepage/index.js
+++
@@ -1,6 +0,0 @@
-const hamburger = document.querySelector(".hamburger_menu input");
-const nav = document.querySelector(".menu");
-
-hamburger.addEventListener('click', function() {
-   nav.classList.toggle('showMenu'); 
-});
